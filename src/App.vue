96aa5ce2365a6e5202c317f1c5e2e6d022a83b24
TheerasakRung <rungworking@gmail.com>
2025-09-30 11:58:49 +0700

    bootstarp

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container text-center mt-5">
+    <h1 class="text-primary mb-4">
+      Hello Vite + Vue + Bootstrap + Font Awesome
+      <i class="fa-solid fa-rocket"></i>
+    </h1>
+
+    <button class="btn btn-success">
+      <i class="fa-solid fa-check"></i> Bootstrap Button
+    </button>
+
+    <div class="mt-4">
+      <i class="fa-solid fa-heart fa-2x text-danger"></i>
+      <i class="fa-solid fa-star fa-2x text-warning"></i>
+    </div>
+    
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
