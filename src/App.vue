411017a6f118a26f43ebb738c06e6354f16c7035
TheerasakRung <rungworking@gmail.com>
2025-09-30 15:06:35 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,16 @@
 <template>
-  <div id="app">
-    <main>
-      <LiffLogin />
-    </main>
-  </div>
+ <div>
+    <LiffLogin />
+ </div>
 </template>
 
 <script setup>
 // Import Component ที่เราสร้างไว้
 import LiffLogin from './components/LiffLogin.vue'; 
+import HeaderMenu from './components/HeaderMenu.vue'; 
+
 </script>
 
 <style>
-/* Style ทั่วไปสำหรับ App */
-#app {
-  text-align: center;
-  color: #2c3e50;
-}
-header {
-  background-color: #00B900;
-  color: white;
-  padding: 10px;
-}
+
 </style>
