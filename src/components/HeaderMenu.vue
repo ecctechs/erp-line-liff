1c487c49af7ccee492481131550af002e9deccc1
TheerasakRung <rungworking@gmail.com>
2025-10-03 16:52:05 +0700

    Update HeaderMenu.vue

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -53,7 +53,7 @@ export default {
       this.profile = JSON.parse(profile);
       this.userData = JSON.parse(userData);
 
-      console.log(this.userData.data)
+      console.log(this.userData.data.business)
     },
   },
   mounted() {
